d03a372d549e2c8bf9aefaee5767123f58243397
zarapatel117 <zarapatel2510@gmail.com~>
2024-12-07 12:54:06 +0530

    count to note

---
+++ count to note.py
@@ -0,0 +1,8 @@
+print ("amount of withdrawl")
+amount=int(input("plese enter the amount of withdrawl "))
+notehundred=amount/100
+notefifty=(amount%100)/50
+noteten=((amount%100)%50)/10
+print(notehundred)
+print(notefifty)
+print(noteten)
