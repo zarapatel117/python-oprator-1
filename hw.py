8d53e3d3d53a299d5aad70d354e64d407a01195c
zarapatel117 <zarapatel2510@gmail.com~>
2024-12-14 15:32:10 +0530

    hw

---
+++ hw.py
@@ -0,0 +1,8 @@
+temprature=int (input("enter the tempreture "))
+
+if temprature > 30:
+    print = str  ("it's a hot day")
+    print("drink plenty of water")
+elif temprature >  20 :
+    print ("it's a nice day")
+    print ("Done")
